82bd1bebdad4211f3f2234a4c25fcdbae7b3dcb3
seollove79 <seollove@gmail.com>
2025-04-24 17:05:42 +0900

    vworld 지도 올리기 완료

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { onMount } from "svelte";
+
+    let mapController;
+    onMount(async () => {
+        vw.MapControllerOption = {
+            container : "vmap",
+            mapMode : "ws3d-map",
+			basemapType : vw.ol3.BasemapType.GRAPHIC,
+			controlDensity : vw.ol3.DensityType.BASIC,
+			interactionDensity : vw.ol3.DensityType.BASIC,
+			controlsAutoArrange : true,
+			homePosition : vw.ol3.CameraPosition,
+			initPosition : vw.ol3.CameraPosition,
+		};
+		mapController = new vw.MapController(vw.MapControllerOption);
+    });
+</script>
+
+<div id="vmap" style="width:100%;height:98%;left:0px;top:0px"></div>
